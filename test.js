a7b4ca6a324cd03d2264f13ae85d3823186c5801
Krzysztof Łyczko <krzysztof.lyczko1@gmail.com>
2021-11-29 14:17:43 +0100

    else if statement added

---
+++ test.js
@@ -0,0 +1,30 @@
+var youLikeMeat = false;
+
+if (youLikeMeat) {
+    document.write(" Here is the meat menu... ");
+} else {
+    document.write(" Here is the vegan menu... ")
+}
+
+var myNum = 8;
+
+if (myNum > 9) {
+    document.write(" myNum is greater than 9...");
+} else {
+    document.write(" myNum is lower than 9...")
+}
+
+var myAge = 21;
+
+if (myAge > 30) {
+    document.write(" you are over 30!...");
+
+} else if (myAge > 20) {
+    document.write(" you are over 20!...");
+
+} else if (myAge > 10) {
+    document.write(" you are over 10!...");
+
+} else {
+    document.write(" you are below minimum age!...")
+}
