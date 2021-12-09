7928758b4090e6f5ba3154a29e39807a37a62f97
Krzysztof Łyczko <krzysztof.lyczko1@gmail.com>
2021-12-09 19:15:51 +0100

    Timers

--- test.js
+++ test.js
@@ -352,7 +352,7 @@ button.onclick = function () {
     }
 }
 
-*/
+
 
 // #41 Window onLoad Event
 
@@ -381,3 +381,32 @@ function setUpEvents() {
 window.onload = function () {
     setUpEvents();
 };
+
+
+*/
+
+// #42 JavaScript Timers
+
+
+var colorChanger = document.getElementById("color-changer");
+var colors = ["red", "blue", "green", "pink"];
+var counter = 0;
+
+function changeColor() {
+
+    if (counter >= colors.length) {
+        counter = 0;
+    }
+
+    colorChanger.style.background = colors[counter];
+    counter++;
+
+}
+
+var myTimer = setInterval(changeColor, 500);
+
+colorChanger.onclick = function () {
+
+    clearInterval(myTimer);
+    colorChanger.innerHTML = "timer stopped";
+}
